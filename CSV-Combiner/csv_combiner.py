d7fa48e86e5f38671ec093253f50a9a14eca211d
yashdafstevens10453472 <74649663+yashdafstevens10453472@users.noreply.github.com>
2021-03-23 17:58:09 -0400

    Add files via upload

--- CSV-Combiner/csv_combiner.py
+++ CSV-Combiner/csv_combiner.py
@@ -1,11 +1,13 @@
 import dask.dataframe as dd
 import pandas as pd
 import sys
-import os.path
 from os import path
+
 def combine(files):
     dataframes,files_new=[],[]
     sysout=sys.stdout
+    
+    # checked if the input has correct extension or nor, if the files exist or not or check extenion is correct or not
     for f in files:
         if f.endswith('.csv'):
             if path.exists(f):
@@ -14,17 +16,20 @@ def combine(files):
                 raise Exception("File does not exist" )
         else:
             raise Exception("incorrect file extension")
+    
+    #check there there is any csv file to combine or not
     if not files_new:
         raise Exception("No files to combine")
-
+    
+    #here the file is read, new column is added, converted to csv and then wrote on outfile 
     for f in files_new:
         file_name = f.split('/')[-1]
         df=dd.read_csv(f)
         df['filename']=file_name
         dataframes.append(df.compute())
     dataframes=pd.concat(dataframes)
-    result=dataframes.to_csv(index=False).replace("\r", "")
-    sysout.write(result)
+    dataframes=dataframes.to_csv(index=False).replace("\r", "")
+    sysout.write(dataframes)
     
 if __name__ == '__main__':
     try:
